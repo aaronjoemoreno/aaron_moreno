5efb3ee0121b3a190879351a3143cbfdc8ec870c
Aaron Moreno <info@aaronmoreno.co>
2020-12-10 08:53:30 -0700

    updated bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -11,7 +11,7 @@ const Bio = () => {
                 <div className="image-container">
                     <img src={profile} alt="Aaron and Family"/>
                 </div>
-                <p className="bio-details">Well, now that you know what I do...let me tell you a little bit more about who I am. I'm Aaron! I am a full stack developer from Albuquerque, NM. I have a beautiful wife named Brittany and full of energy son named Micah. I love to teach others. I used to be a trainer for Apple. I share ways code tutorials here: <a href="https://www.instagram.com/allthejavascript" target="blank">All The JS.</a> I enjoy running, cooking, watching good movies, especially ones from the 80s and producing music. I also do a little blogging, which you can find below.</p>
+                <p className="bio-details">Well, now that you know what I do...let me tell you a little bit more about who I am. I'm Aaron! I am a full stack developer from Albuquerque, NM. I have a beautiful wife named Brittany and full of energy son named Micah. I love to teach others. I used to be a trainer for Apple. I share simple code tutorials here: <a href="https://www.instagram.com/allthejavascript" target="blank" style={{fontSize: "2rem", textDecoration: "none", color: "black"}}>All The JS.</a> I enjoy running, cooking, watching good movies, especially ones from the 80s and producing music.</p>
             </div>
         </>
     )
